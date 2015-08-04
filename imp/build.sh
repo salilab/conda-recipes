eeb6e6a9a0700d3968373fe9a39f2ceccaf0be3c
Ben Webb <ben@salilab.org>
2015-08-03 23:07:10 -0700

    Install in /lib on 64-bit, not /lib64.

--- imp/build.sh
+++ imp/build.sh
@@ -6,6 +6,7 @@ export CMAKE_PREFIX_PATH=${PREFIX}
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DIMP_DISABLED_MODULES=scratch \
       -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+      -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
       -DCMAKE_INSTALL_PYTHONDIR=${SP_DIR} ..
 make -j4
 make install
