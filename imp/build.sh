dc1bd5a35e839b59dd4298def8f8f1649348a97b
Ben Webb <ben@salilab.org>
2015-08-07 22:33:30 -0700

    Build Boost on 10.6 Mac so we can build IMP.
    The standard conda boost package is built with clang++ and requires
    OS X 10.8 or later. But if we try to build IMP on 10.8 or later it
    1) won't work on 10.6, the floor for Anaconda; and
    2) insists on building with C++11, which again doesn't work because
       the conda boost package is built without C++11 (and the two cannot
       be mixed).
    Fix this by building our own boost package, compiled with g++, on 10.6,
    and use it as an IMP dependency. Relates salilab/imp#858.

--- imp/build.sh
+++ imp/build.sh
@@ -3,10 +3,20 @@
 # Find packages in Anaconda locations
 export CMAKE_PREFIX_PATH=${PREFIX}
 
+if [ `uname -s` = "Darwin" ]; then
+  EXTRA_CMAKE_FLAGS="\
+      -DCMAKE_INCLUDE_PATH=${PREFIX}/include/boost-mac \
+      -DCMAKE_LIBRARY_PATH=${PREFIX}/lib/boost-mac \
+      -DPYTHON_INCLUDE_DIR=${PREFIX}/include/python${PY_VER}"
+else
+  EXTRA_CMAKE_FLAGS=""
+fi
+
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release -DIMP_DISABLED_MODULES=scratch \
       -DCMAKE_INSTALL_PREFIX=${PREFIX} \
       -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
-      -DCMAKE_INSTALL_PYTHONDIR=${SP_DIR} ..
+      -DCMAKE_INSTALL_PYTHONDIR=${SP_DIR} \
+      ${EXTRA_CMAKE_FLAGS} ..
 make -j4
 make install
