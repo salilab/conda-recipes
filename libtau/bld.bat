97014197aba4a34f787eb3c54f07bf456e663c87
Ben Webb <ben@salilab.org>
2015-08-10 13:07:04 -0700

    Add build script for Windows.

--- libtau/bld.bat
+++ libtau/bld.bat
@@ -1,4 +1,14 @@
-"%PYTHON%" setup.py install
+if %ARCH% == "64" (
+  set TAU_LIB="Windows.x86_64"
+) else (
+  set TAU_LIB="Windows.i386"
+)
+
+mkdir %LIBRARY_INC%\libTAU
+copy lib\%TAU_LIB%\*.dll %LIBRARY_LIB%
+copy lib\%TAU_LIB%\*.lib %LIBRARY_LIB%
+copy include\*.h %LIBRARY_INC%\libTAU
+
 if errorlevel 1 exit 1
 
 :: Add more build steps here, if they are necessary.
