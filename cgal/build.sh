f940c3a300e4f21d5fed04e7f233e7bb8caae0ef
Ben Webb <ben@salilab.org>
2015-08-10 11:58:12 -0700

    Fix build on Mac OS X 10.6.

--- cgal/build.sh
+++ cgal/build.sh
@@ -5,10 +5,10 @@ export CMAKE_PREFIX_PATH=${PREFIX}
 
 if [ `uname` == Darwin ]; then
     cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
-    -D BOOST_ROOT=$PREFIX \
-    -D BOOST_INCLUDE_DIR=$PREFIX/include \
+    -DCMAKE_INCLUDE_PATH=${PREFIX}/include/boost-mac \
+    -DCMAKE_LIBRARY_PATH=${PREFIX}/lib/boost-mac \
     -D MPFR_INCLUDE_DIR=$PREFIX/include \
-    -D MPFR_LIBRARIES=$PREFIX/lib/libmpfr.dylib \
+    -D MPFR_LIBRARIES=$PREFIX/lib/libmpfr.a \
     -D GMP_INCLUDE_DIR=$PREFIX/include \
     -D GMP_LIBRARIES=$PREFIX/lib/libgmp.dylib \
     -D GMP_LIBRARIES_DIR=$PREFIX/lib \
@@ -16,8 +16,8 @@ if [ `uname` == Darwin ]; then
     -D GMPXX_LIBRARIES=$PREFIX/include/libgmpxx.dylib \
     -D ZLIB_LIBRARY=$PREFIX/lib/libz.dylib \
     -D ZLIB_INCLUDE_DIR=$PREFIX/include \
-    -D WITH_OpenGL=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.10.sdk/System/Library/Frameworks/OpenGL.framework/ \
-	-D OPENGL_INCLUDE_DIR=/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.10.sdk/System/Library/Frameworks/OpenGL.framework/Headers \
+    -D WITH_OpenGL=/System/Library/Frameworks/OpenGL.framework/ \
+    -D OPENGL_INCLUDE_DIR=/System/Library/Frameworks/OpenGL.framework/Headers \
     .
 else
     cmake -D CMAKE_INSTALL_PREFIX=$PREFIX \
