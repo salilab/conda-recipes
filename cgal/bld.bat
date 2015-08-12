3b6ec581ff0ec1920527a87f62a9e23da285670c
Ben Webb <ben@salilab.org>
2015-08-11 18:43:51 -0700

    Add start of Windows support.

---
+++ cgal/bld.bat
@@ -0,0 +1,15 @@
+:: Find packages in Anaconda locations
+set CMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
+
+cmake -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DBOOST_ROOT=%LIBRARY_PREFIX% -DBOOST_INCLUDE_DIR=%LIBRARY_INC% -DZLIB_LIBRARY=%LIBRARY_LIB%\zlib.lib -DZLIB_INCLUDE_DIR=%LIBRARY_INC% -G"NMake Makefiles" .
+
+nmake
+nmake install
+
+if errorlevel 1 exit 1
+
+:: Add more build steps here, if they are necessary.
+
+:: See
+:: http://docs.continuum.io/conda/build.html
+:: for a list of environment variables that are set during the build process.
