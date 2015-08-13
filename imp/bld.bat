5fbec000046c1238adc634f66fdcfe1dbddd960b
Ben Webb <ben@salilab.org>
2015-08-13 14:50:31 -0700

    Start adding Windows support.

--- imp/bld.bat
+++ imp/bld.bat
@@ -1,4 +1,18 @@
-"%PYTHON%" setup.py install
+::# Find packages in Anaconda locations
+set CMAKE_PREFIX_PATH=%LIBRARY%
+
+:: Fix cmake paths to use / rather than \
+patch -p1 < %RECIPE_DIR%\cmake-path.patch
+if errorlevel 1 exit 1
+
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release -DIMP_DISABLED_MODULES=scratch -DCMAKE_INSTALL_PREFIX="%LIBRARY%" -DCMAKE_INSTALL_PYTHONDIR="%SP_DIR%" -G "NMake Makefiles" ..
+if errorlevel 1 exit 1
+nmake
+if errorlevel 1 exit 1
+nmake install
+
 if errorlevel 1 exit 1
 
 :: Add more build steps here, if they are necessary.
