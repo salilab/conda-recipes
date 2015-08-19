f190841ae0a2c5c093f3e5bf1c0f5086b2a09795
Ben Webb <ben@salilab.org>
2015-08-19 13:53:18 -0700

    Add wrapper for each command line tool.

--- imp/bld.bat
+++ imp/bld.bat
@@ -40,6 +40,11 @@ mkdir tools\dev_tools\python_tools
 copy modules\rmf\dependency\RMF\tools\dev_tools\python_tools\* tools\dev_tools\python_tools\
 if errorlevel 1 exit 1
 
+:: build app wrapper
+copy "%RECIPE_DIR%/app_wrapper.c" .
+cl app_wrapper.c shell32.lib
+if errorlevel 1 exit 1
+
 mkdir build
 cd build
 cmake -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" -DCMAKE_BUILD_TYPE=Release -DIMP_DISABLED_MODULES=scratch -DHDF5_C_LIBRARY="%LIBRARY_LIB%\hdf5.lib" -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" -DCMAKE_INSTALL_PYTHONDIR="%SP_DIR%" -DCMAKE_CXX_FLAGS="/DBOOST_ALL_DYN_LINK /EHsc /D_HDF5USEDLL_ /DWIN32 /DGSL_DLL %EXTRA_CXX_FLAGS%" -DOPENCV22_LIBRARIES="%LIBRARY_LIB%\opencv_core249.lib;%LIBRARY_LIB%\opencv_imgproc249.lib;%LIBRARY_LIB%\opencv_highgui249.lib;%LIBRARY_LIB%\opencv_contrib249.lib" -G "NMake Makefiles" ..
@@ -52,6 +57,16 @@ if errorlevel 1 exit 1
 
 :: Patch IMP Python module to add paths containing Anaconda DLLs to search path
 python "%RECIPE_DIR%\add_dll_search_path.py" "%SP_DIR%\IMP" "%LIBRARY_BIN%" "%LIBRARY_LIB%" "%SP_DIR%\IMP\__init__.py"
+if errorlevel 1 exit 1
+
+:: Add wrappers to path for each command line tool
+cd bin
+:: Handle Python tools (all files without an extension)
+for /f %%f in ('dir /b *.') do copy "%SRC_DIR%\app_wrapper.exe" "%PREFIX%\%%f.exe"
+if errorlevel 1 exit 1
+
+:: Handle C++ tools (all files with .exe extension)
+for /f %%f in ('dir /b *.exe') do copy "%SRC_DIR%\app_wrapper.exe" "%PREFIX%\%%f"
 if errorlevel 1 exit 1
 
 :: Add more build steps here, if they are necessary.
