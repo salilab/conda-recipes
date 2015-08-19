985d602279c75121039ee277825074ecaff60afb
Ben Webb <ben@salilab.org>
2015-08-19 14:12:42 -0700

    Set correct machine type on 64 bit.

--- fftw/bld.bat
+++ fftw/bld.bat
@@ -1,7 +1,13 @@
+if "%ARCH%" == "64" (
+  set MACHINE=X64
+) else (
+  set MACHINE=X86
+)
+
 :: make MSVC .lib files from .def
-lib /def:libfftw3-3.def
-lib /def:libfftw3f-3.def
-lib /def:libfftw3l-3.def
+lib /MACHINE:%MACHINE% /def:libfftw3-3.def
+lib /MACHINE:%MACHINE% /def:libfftw3f-3.def
+lib /MACHINE:%MACHINE% /def:libfftw3l-3.def
 
 copy libfftw3-3.dll "%LIBRARY_LIB%\"
 copy libfftw3f-3.dll "%LIBRARY_LIB%\"
