666eeab71f791598f43fd1308caa61342b79a9ac
Ben Webb <ben@salilab.org>
2015-08-21 11:17:38 -0700

    Need to use same build flags as Boost.

--- imp/build.sh
+++ imp/build.sh
@@ -4,14 +4,24 @@
 export CMAKE_PREFIX_PATH=${PREFIX}
 
 if [ `uname -s` = "Darwin" ]; then
+  # -stdlib=libc++ is not compatible with 10.6 (and Anaconda dropped support
+  # for 10.6 anyway)
+  export MACOSX_DEPLOYMENT_TARGET=10.7
   PYINC=`echo ${PREFIX}/include/python${PY_VER}*`
+  # Use clang and libc++ library otherwise linking against Boost libraries
+  # will fail
   EXTRA_CMAKE_FLAGS="\
+      -DCMAKE_C_COMPILER=clang \
+      -DCMAKE_CXX_COMPILER=clang++ \
       -DPYTHON_INCLUDE_DIR=${PYINC}"
   # Make sure the default encoding for files opened by Python 3 is UTF8
   export LANG=en_US.UTF-8
-  # Help ld to find opencv libs
-  export LDFLAGS="-L${PREFIX}/lib"
+  # Help ld to find opencv libs; use clang++
+  export LDFLAGS="-L${PREFIX}/lib -stdlib=libc++"
+  export CXXFLAGS="-std=c++11 -stdlib=libc++"
+  NCPU=$(sysctl -n hw.ncpu)
 else
+  NCPU=4
   EXTRA_CMAKE_FLAGS=""
 fi
 
@@ -21,5 +31,5 @@ cmake -DCMAKE_BUILD_TYPE=Release -DIMP_DISABLED_MODULES=scratch \
       -DCMAKE_INSTALL_LIBDIR=${PREFIX}/lib \
       -DCMAKE_INSTALL_PYTHONDIR=${SP_DIR} \
       ${EXTRA_CMAKE_FLAGS} ..
-make -j4
+make -j${NCPU}
 make install
