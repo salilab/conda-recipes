59dfc18a4ecf412b267b35c9f30b8ae3c23db82b
Ben Webb <ben@salilab.org>
2015-08-20 18:03:07 -0700

    Build CGAL for Windows.

--- cgal/bld.bat
+++ cgal/bld.bat
@@ -1,9 +1,11 @@
 :: Find packages in Anaconda locations
 set CMAKE_PREFIX_PATH=%LIBRARY_PREFIX%
 
-cmake -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% -DBOOST_ROOT=%LIBRARY_PREFIX% -DBOOST_INCLUDE_DIR=%LIBRARY_INC% -DZLIB_LIBRARY=%LIBRARY_LIB%\zlib.lib -DZLIB_INCLUDE_DIR=%LIBRARY_INC% -G"NMake Makefiles" .
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" -DBOOST_ROOT="%LIBRARY_PREFIX%" -DBOOST_INCLUDE_DIR="%LIBRARY_INC%" -DZLIB_LIBRARY="%LIBRARY_LIB%\zlib.lib" -DZLIB_INCLUDE_DIR="%LIBRARY_INC%" -G"NMake Makefiles" .
+if errorlevel 1 exit 1
 
 nmake
+if errorlevel 1 exit 1
 nmake install
 
 if errorlevel 1 exit 1
