cde38fbe1419f223b723d852042bed36fd2ca030
Ben Webb <ben@salilab.org>
2015-08-21 16:03:38 -0700

    Start adding support for Windows.

--- modeller/bld.bat
+++ modeller/bld.bat
@@ -1,8 +1,70 @@
-"%PYTHON%" setup.py install
+set MODTOP=%LIBRARY_PREFIX%\modeller
+if "%ARCH%" == "64" (
+  set EXETYPE=x86_64-w64
+) else (
+  set EXETYPE=i386-w32
+)
+
+move modlib\modeller "%SP_DIR%\"
 if errorlevel 1 exit 1
 
-:: Add more build steps here, if they are necessary.
+move lib\%EXETYPE%\python%PY_VER%\_modeller.pyd "%SP_DIR%\"
+if errorlevel 1 exit 1
 
-:: See
-:: http://docs.continuum.io/conda/build.html
-:: for a list of environment variables that are set during the build process.
+move lib\%EXETYPE%\libmodeller*.dll "%PREFIX%\"
+if errorlevel 1 exit 1
+move lib\%EXETYPE%\libsaxs.dll "%PREFIX%\"
+if errorlevel 1 exit 1
+move lib\%EXETYPE%\libifcoremd.dll "%PREFIX%\"
+if errorlevel 1 exit 1
+move lib\%EXETYPE%\libmmd.dll "%PREFIX%\"
+if errorlevel 1 exit 1
+
+:: iconv, hdf5_hl, hdf5, intl, libglib-2.0-0, zlib1 copy but need to rename
+:: so we don't conflict with other DLLs (e.g. the conda hdf5 package)
+
+if "%ARCH%" == "32" (
+  move lib\%EXETYPE%\iconv.dll "%PREFIX%\"
+  if errorlevel 1 exit 1
+  move lib\%EXETYPE%\intl.dll "%PREFIX%\"
+  if errorlevel 1 exit 1
+) else (
+  move lib\%EXETYPE%\libintl-8.dll "%PREFIX%\"
+  if errorlevel 1 exit 1
+)
+move lib\%EXETYPE%\hdf5.dll "%PREFIX%\"
+if errorlevel 1 exit 1
+move lib\%EXETYPE%\hdf5_hl.dll "%PREFIX%\"
+if errorlevel 1 exit 1
+move lib\%EXETYPE%\libglib-2.0-0.dll "%PREFIX%\"
+if errorlevel 1 exit 1
+
+move lib\%EXETYPE%\mod*.exe "%PREFIX%\"
+if errorlevel 1 exit 1
+move lib\%EXETYPE%\python23.dll "%PREFIX%\"
+if errorlevel 1 exit 1
+
+:: 64-bit uses msvcr110, so must pull that in (not in Anaconda)
+
+mkdir "%MODTOP%"
+if errorlevel 1 exit 1
+
+move ChangeLog "%MODTOP%\ChangeLog.txt"
+if errorlevel 1 exit 1
+move README.txt "%MODTOP%\"
+if errorlevel 1 exit 1
+move modlib "%MODTOP%\"
+if errorlevel 1 exit 1
+move src "%MODTOP%\"
+if errorlevel 1 exit 1
+move bin "%MODTOP%\"
+if errorlevel 1 exit 1
+move doc "%MODTOP%\"
+if errorlevel 1 exit 1
+move examples "%MODTOP%\"
+if errorlevel 1 exit 1
+
+:: Make config.py (note; path is wrong in Modeller error message if license
+:: is incorrect)
+echo install_dir = r'%MODTOP%' > "%SP_DIR%\modeller\config.py"
+echo license = r'XXXX' >> "%SP_DIR%\modeller\config.py"
---
+++ modeller/post-link.bat
@@ -0,0 +1,14 @@
+@echo off
+set MODTOP=%PREFIX%\Library\modeller
+set CONFIG=%PREFIX%\lib\site-packages\modeller\config.py
+
+if "%KEY_MODELLER%" == "" (
+  echo
+  echo Edit %CONFIG%
+  echo and replace XXXX with your Modeller license key,
+  echo or set the KEY_MODELLER environment variable before running 'conda install'.
+  echo
+) else (
+  echo install_dir = r'%MODTOP%' > "%CONFIG%"
+  echo license = r'%KEY_MODELLER%' >> "%CONFIG%"
+)
