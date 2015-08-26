54db47485bdc4935790fbbac2777c838b4d2ace3
Ben Webb <ben@salilab.org>
2015-08-26 16:03:34 -0700

    Rename DLLs to avoid conflicts.

--- modeller/bld.bat
+++ modeller/bld.bat
@@ -5,44 +5,31 @@ if "%ARCH%" == "64" (
   set EXETYPE=i386-w32
 )
 
-move lib\%EXETYPE%\python%PY_VER%\_modeller.pyd "%SP_DIR%\"
+set SOVERSION=10
+
+:: temporarily rename to avoid conflict with python%PY_VER%\_modeller.pyd
+move lib\%EXETYPE%\python2.3\_modeller.pyd lib\%EXETYPE%\python2.3\_modeller23.pyd
 if errorlevel 1 exit 1
 
-move lib\%EXETYPE%\libmodeller*.dll "%PREFIX%\"
-if errorlevel 1 exit 1
-move lib\%EXETYPE%\libsaxs.dll "%PREFIX%\"
-if errorlevel 1 exit 1
-move lib\%EXETYPE%\libifcoremd.dll "%PREFIX%\"
-if errorlevel 1 exit 1
-move lib\%EXETYPE%\libmmd.dll "%PREFIX%\"
-if errorlevel 1 exit 1
-
-:: iconv, hdf5_hl, hdf5, intl, libglib-2.0-0, zlib1 copy but need to rename
-:: so we don't conflict with other DLLs (e.g. the conda hdf5 package)
-
+:: Install bundled DLLs but rename them to avoid potential conflicts with
+:: other Anaconda packages (e.g. the conda hdf5 package). We do this because
+:: DLLs either need to be in the PATH or in the same directory as the binary
+:: that uses them. We can't do the latter because the modXXX binary, the
+:: libmodeller DLL, and the two identically named _modeller.pyd extensions
+:: cannot all be in the same directory, and if we put the DLLs in the PATH, they
+:: are visible globally and could conflict.
 if "%ARCH%" == "32" (
-  move lib\%EXETYPE%\iconv.dll "%PREFIX%\"
-  if errorlevel 1 exit 1
-  move lib\%EXETYPE%\intl.dll "%PREFIX%\"
+  python "%RECIPE_DIR%\install_bins.py" lib\%EXETYPE% "%PREFIX%" iconv.dll intl.dll hdf5.dll hdf5_hl.dll libglib-2.0-0.dll zlib1.dll libifcoremd.dll libmmd.dll --norename libmodeller%SOVERSION%.dll libsaxs.dll mod%PKG_VERSION%.exe python%PY_VER%\_modeller.pyd python2.3\_modeller23.pyd
   if errorlevel 1 exit 1
 ) else (
-  move lib\%EXETYPE%\libintl-8.dll "%PREFIX%\"
+  :: 64-bit uses msvcr110, so must pull that in (not in Anaconda)
+  python "%RECIPE_DIR%\install_bins.py" lib\%EXETYPE% "%PREFIX%" libintl-8.dll hdf5.dll hdf5_hl.dll libglib-2.0-0.dll zlib1.dll libifcoremd.dll libmmd.dll --norename msvcr110.dll libmodeller%SOVERSION%.dll libsaxs.dll mod%PKG_VERSION%.exe python%PY_VER%\_modeller.pyd python2.3\_modeller23.pyd
   if errorlevel 1 exit 1
 )
-move lib\%EXETYPE%\hdf5.dll "%PREFIX%\"
-if errorlevel 1 exit 1
-move lib\%EXETYPE%\hdf5_hl.dll "%PREFIX%\"
-if errorlevel 1 exit 1
-move lib\%EXETYPE%\libglib-2.0-0.dll "%PREFIX%\"
-if errorlevel 1 exit 1
-move lib\%EXETYPE%\zlib1.dll "%PREFIX%\"
-if errorlevel 1 exit 1
 
-:: 64-bit uses msvcr110, so must pull that in (not in Anaconda)
-if "%ARCH%" == "64" (
-  move lib\%EXETYPE%\msvcr110.dll "%PREFIX%\"
-  if errorlevel 1 exit 1
-)
+:: Put _modeller.pyd in correct location
+move "%PREFIX%\_modeller.pyd" "%SP_DIR%\"
+if errorlevel 1 exit 1
 
 mkdir "%MODTOP%"
 if errorlevel 1 exit 1
@@ -74,12 +61,13 @@ echo install_dir = r'%MODTOP%' > "%MODTOP%\modlib\modeller\config.py"
 echo license = r'XXXX' >> "%MODTOP%\modlib\modeller\config.py"
 
 :: make mini Python 2.3 environment so modXXX binary works
-move lib\%EXETYPE%\mod%PKG_VERSION%.exe "%MODTOP%\modlib\mod%PKG_VERSION%-orig.exe"
+move "%PREFIX%\mod%PKG_VERSION%.exe" "%MODTOP%\modlib\mod%PKG_VERSION%-orig.exe"
 if errorlevel 1 exit 1
 move lib\%EXETYPE%\python23.dll "%MODTOP%\modlib\"
 if errorlevel 1 exit 1
 mkdir "%MODTOP%\modlib\lib"
-move lib\%EXETYPE%\python2.3\_modeller.pyd "%MODTOP%\modlib\lib\"
+move "%PREFIX%\_modeller23.pyd" "%MODTOP%\modlib\lib\_modeller.pyd"
+if errorlevel 1 exit 1
 
 :: make modXXX wrapper that sets MODINSTALLXXX and PYTHONHOME then calls
 :: real modXXX binary
---
+++ modeller/install_bins.py
@@ -0,0 +1,33 @@
+from __future__ import print_function
+import sys
+import os
+
+def install(src, dest, rename_map):
+    print("Install %s -> %s" % (src, dest))
+    with open(src, 'rb') as fh:
+        contents = fh.read()
+    for orig, repl in rename_map.items():
+        contents = contents.replace(orig, repl)
+    with open(dest, 'wb') as fh:
+        fh.write(contents)
+
+source_dir = sys.argv[1]
+dest_dir = sys.argv[2]
+norename_index = sys.argv.index('--norename')
+
+norename = sys.argv[norename_index+1:]
+rename = sys.argv[3:norename_index]
+
+rename_map = {}
+for r in rename:
+    root, ext = os.path.splitext(r)
+    new_fname = '_m' + root[:-2] + ext
+    print("Renaming %s to %s" % (r, new_fname))
+    rename_map[r] = new_fname
+
+for r in rename:
+    install(os.path.join(source_dir, r), os.path.join(dest_dir, rename_map[r]),
+            rename_map)
+for r in norename:
+    install(os.path.join(source_dir, r),
+            os.path.join(dest_dir, os.path.basename(r)), rename_map)
