971e08123bbc5eca138a2d60681b5d9c42fa03ab
Ben Webb <ben@salilab.org>
2015-08-28 07:24:31 -0700

    Delete Miniconda download after we're done.

--- build/linux-32/setup.sh
+++ build/linux-32/setup.sh
@@ -7,6 +7,7 @@ mock -r $CONFIG --install gcc-c++ make bzip2 wget openssh-clients perl gcc44 gcc
 wget https://repo.continuum.io/miniconda/Miniconda-3.10.1-Linux-x86.sh && echo "509ee56f1590705472fdac4a00aa7191f79a6a09daf4af088e92f93c648d815e  Miniconda-3.10.1-Linux-x86.sh" | sha256sum -c --status || exit 1
 mock -r $CONFIG --copyin Miniconda-3.10.1-Linux-x86.sh / || exit 1
 mock -r $CONFIG --chroot "bash /Miniconda-3.10.1-Linux-x86.sh -b -p /root/miniconda && rm -f /Miniconda-3.10.1-Linux-x86.sh" || exit 1
+rm -f Miniconda-3.10.1-Linux-x86.sh
 mock -r $CONFIG --chroot 'echo "export PATH=\"/root/miniconda/bin:\$PATH\"" >> /root/.bashrc' || exit 1
 
 echo "Use 'mock -r $CONFIG --copyin' to copy files into the mock chroot"
