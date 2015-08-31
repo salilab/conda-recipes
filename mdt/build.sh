dbbb76cded4bd62cbc0f46e0c1724498309f2499
Ben Webb <ben@salilab.org>
2015-08-31 11:52:06 -0700

    Fix SWIG and install paths.

--- mdt/build.sh
+++ mdt/build.sh
@@ -3,4 +3,6 @@
 HDF5_VERSION="1813"
 
 scons -j4 prefix=$PREFIX includepath=$PREFIX/include/hdf5-$HDF5_VERSION \
-          libpath=$PREFIX/lib/hdf5-$HDF5_VERSION install
+          libpath=$PREFIX/lib/hdf5-$HDF5_VERSION \
+          pythondir=$SP_DIR libdir=$PREFIX/lib \
+          path=$PREFIX/bin install
