c96d6d5d9bd7a0c3cc2242831ba549d6e99d77d9
Ben Webb <ben@salilab.org>
2015-09-15 15:21:27 -0700

    Fix echo of blank lines.

--- modeller/post-link.bat
+++ modeller/post-link.bat
@@ -3,11 +3,11 @@ set MODTOP=%PREFIX%\Library\modeller
 set CONFIG=%MODTOP%\modlib\modeller\config.py
 
 if "%KEY_MODELLER%" == "" (
-  echo
+  echo(
   echo Edit %CONFIG%
   echo and replace XXXX with your Modeller license key,
   echo or set the KEY_MODELLER environment variable before running 'conda install'.
-  echo
+  echo(
 ) else (
   echo install_dir = r'%MODTOP%' > "%CONFIG%"
   echo license = r'%KEY_MODELLER%' >> "%CONFIG%"
