69d82e2e1478a8d82e562d69a190b3967ecb12d4
Ben Webb <ben@salilab.org>
2015-12-08 22:37:53 -0800

    Fix creation of RMF files in Linux package.
    The combination of new Boost (specifically, the container::flat_map
    and container::flat_set types) and the old gcc 4.1 resulted in RMF
    generating corrupt output files, missing some of the node attributes.
    Amongst other things, this caused the tutorial to fail. Fix this by
    building with the newer gcc 4.4 instead, and add a test to make sure
    that we can read back in RMF files that we create.

--- imp/run_test.py
+++ imp/run_test.py
@@ -1,3 +1,28 @@
+import IMP
+import IMP.core
+import IMP.atom
+import IMP.algebra
+import IMP.rmf
 import IMP.test
+import RMF
+import os
+
 # Make sure that install prefix is set correctly
 IMP.test.get_data_path('linux.words')
+
+# Make sure that we can read in an RMF file that we ourselves created
+m = IMP.Model()
+d = IMP.core.XYZR.setup_particle(IMP.Particle(m),
+                  IMP.algebra.Sphere3D(IMP.algebra.Vector3D(1,2,3), 2.0))
+IMP.atom.Mass.setup_particle(d, 4.0)
+
+r = RMF.create_rmf_file("test.rmf")
+IMP.rmf.add_hierarchies(r, [d])
+IMP.rmf.save_frame(r)
+del r
+
+r = RMF.open_rmf_file_read_only("test.rmf")
+IMP.rmf.link_hierarchies(r, [d])
+IMP.rmf.load_frame(r, 0)
+
+os.unlink("test.rmf")
