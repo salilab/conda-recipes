d25ce3246e7fde8d08b98ec92c6efe223f78f22f
Ben Webb <ben@salilab.org>
2015-12-15 16:30:54 -0800

    Find Boost headers/libraries on Mac.

--- dssp/build.sh
+++ dssp/build.sh
@@ -7,7 +7,15 @@ perl -pi -e 's/\-static //' makefile
 
 echo "DEST_DIR=${PREFIX}" > make.config
 echo "MAN_DIR=${PREFIX}/share/man/man1" >> make.config
-echo "BOOST_INC_DIR=${PREFIX}/include" >> make.config
-echo "BOOST_LIB_DIR=${PREFIX}/lib" >> make.config
+
+if [ `uname -s` = "Darwin" ]; then
+  echo "BOOST_INC_DIR=${PREFIX}/include/boost-mac" >> make.config
+  echo "BOOST_LIB_DIR=${PREFIX}/lib/boost-mac" >> make.config
+  perl -pi -e "s#^(CFLAGS.*)#\$1 -I${PREFIX}/include#" makefile
+  perl -pi -e "s#^(LDOPTS.*)#\$1 -L${PREFIX}/lib#" makefile
+else
+  echo "BOOST_INC_DIR=${PREFIX}/include" >> make.config
+  echo "BOOST_LIB_DIR=${PREFIX}/lib" >> make.config
+fi
 
 make -j4 install
