de758f4bc1b338ad5de4feebf23188f293537cb7
Ben Webb <ben@salilab.org>
2016-01-13 19:01:04 -0800

    Pull in vim-common to get xxd binary.
    The xxd binary is used by the SWIG conda package.

--- build/linux-32/setup.sh
+++ build/linux-32/setup.sh
@@ -3,7 +3,7 @@
 CONFIG=epel-5-i386
 
 mock -r $CONFIG --init || exit 1
-mock -r $CONFIG --install gcc-c++ make bzip2 wget openssh-clients perl gcc44 gcc44-c++ pkgconfig || exit 1
+mock -r $CONFIG --install gcc-c++ make bzip2 wget openssh-clients perl gcc44 gcc44-c++ pkgconfig vim-common || exit 1
 wget https://repo.continuum.io/miniconda/Miniconda-3.10.1-Linux-x86.sh && echo "509ee56f1590705472fdac4a00aa7191f79a6a09daf4af088e92f93c648d815e  Miniconda-3.10.1-Linux-x86.sh" | sha256sum -c --status || exit 1
 mock -r $CONFIG --copyin Miniconda-3.10.1-Linux-x86.sh / || exit 1
 mock -r $CONFIG --chroot "bash /Miniconda-3.10.1-Linux-x86.sh -b -p /root/miniconda && rm -f /Miniconda-3.10.1-Linux-x86.sh" || exit 1
