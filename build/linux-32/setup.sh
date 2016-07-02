6f6071c14df1398eb6f6361df2051f19b354187f
Ben Webb <ben@salilab.org>
2016-07-01 17:59:51 -0700

    Switch 32-bit Linux build environment to Python 3.

--- build/linux-32/setup.sh
+++ build/linux-32/setup.sh
@@ -4,10 +4,10 @@ CONFIG=epel-5-i386
 
 mock -r $CONFIG --init || exit 1
 mock -r $CONFIG --install gcc-c++ make bzip2 wget openssh-clients perl gcc44 gcc44-c++ pkgconfig vim-common || exit 1
-wget https://repo.continuum.io/miniconda/Miniconda-3.10.1-Linux-x86.sh && echo "509ee56f1590705472fdac4a00aa7191f79a6a09daf4af088e92f93c648d815e  Miniconda-3.10.1-Linux-x86.sh" | sha256sum -c --status || exit 1
-mock -r $CONFIG --copyin Miniconda-3.10.1-Linux-x86.sh / || exit 1
-mock -r $CONFIG --chroot "bash /Miniconda-3.10.1-Linux-x86.sh -b -p /root/miniconda && rm -f /Miniconda-3.10.1-Linux-x86.sh" || exit 1
-rm -f Miniconda-3.10.1-Linux-x86.sh
+wget https://repo.continuum.io/miniconda/Miniconda3-4.0.5-Linux-x86.sh && echo "3c06b31b0f70d21f4f62021b8db98929faa3a99ebe6b5b1a2999576d16c30e35  Miniconda3-4.0.5-Linux-x86.sh" | sha256sum -c --status || exit 1
+mock -r $CONFIG --copyin Miniconda3-4.0.5-Linux-x86.sh / || exit 1
+mock -r $CONFIG --chroot "bash /Miniconda3-4.0.5-Linux-x86.sh -b -p /root/miniconda && rm -f /Miniconda3-4.0.5-Linux-x86.sh" || exit 1
+rm -f Miniconda3-4.0.5-Linux-x86.sh
 mock -r $CONFIG --chroot 'echo "export PATH=\"/root/miniconda/bin:\$PATH\"" >> /builddir/.bashrc' || exit 1
 mock -r $CONFIG --chroot 'source /builddir/.bashrc && conda update -y --all && conda install -y conda-build && conda config --add channels salilab' || exit 1
 
