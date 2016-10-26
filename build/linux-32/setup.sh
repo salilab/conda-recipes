acf047b269d3054e8a3b3a6317ee4247d847be7a
Ben Webb <ben@salilab.org>
2016-10-26 11:11:56 -0700

    Latest conda build needs these extra packages.
    Without these packages installed conda build tries
    to install them and, in the process, forgets to
    install the packages needed to build the current
    package.

--- build/linux-32/setup.sh
+++ build/linux-32/setup.sh
@@ -9,7 +9,7 @@ mock -r $CONFIG --copyin Miniconda3-4.0.5-Linux-x86.sh / || exit 1
 mock -r $CONFIG --chroot "bash /Miniconda3-4.0.5-Linux-x86.sh -b -p /root/miniconda && rm -f /Miniconda3-4.0.5-Linux-x86.sh" || exit 1
 rm -f Miniconda3-4.0.5-Linux-x86.sh
 mock -r $CONFIG --chroot 'echo "export PATH=\"/root/miniconda/bin:\$PATH\"" >> /builddir/.bashrc' || exit 1
-mock -r $CONFIG --chroot 'source /builddir/.bashrc && conda update -y --all && conda install -y conda-build && conda config --add channels salilab' || exit 1
+mock -r $CONFIG --chroot 'source /builddir/.bashrc && conda update -y --all && conda install -y conda-build curl git expat openssl && conda config --add channels salilab' || exit 1
 
 echo "Use 'mock -r $CONFIG --copyin' to copy files into the mock chroot"
 echo "and 'mock -r $CONFIG --shell' to get a conda shell."
