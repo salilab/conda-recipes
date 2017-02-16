540ad7c069cd6179461c8b5827e1c6260f4ded9f
Ben Webb <ben@salilab.org>
2017-02-16 14:59:06 -0800

    Make sure we're not affected by a conda-build bug.

---
+++ modeller/run_test.py
@@ -0,0 +1,15 @@
+import os
+
+# Make sure that expected symlinks are there
+# Certain versions of conda-build break our symlinks and replace them with
+# copies. This makes the package much larger and also breaks setting up
+# the license key at post-install. conda-build 2.1 seems to be affected
+# but conda-build 2.0.10 seems OK.
+
+p = os.path.join(os.environ['SP_DIR'], 'modeller')
+assert(os.path.islink(p) and os.path.exists(p))
+
+prefix = os.environ['CONDA_PREFIX']
+ver = os.environ['PKG_VERSION']
+p = os.path.join(prefix, 'lib', 'modeller-%s' % ver, 'src', 'include')
+assert(os.path.islink(p) and os.path.exists(p))
