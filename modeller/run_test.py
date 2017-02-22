e77d4210c78b913e98422459ed918352df51cd45
Ben Webb <ben@salilab.org>
2017-02-22 14:43:06 -0800

    Skip symlink check on Windows.
    The Windows package doesn't contain any symlinks, so
    it makes no sense to check for them.

--- modeller/run_test.py
+++ modeller/run_test.py
@@ -1,10 +1,15 @@
 import os
+import sys
 
 # Make sure that expected symlinks are there
 # Certain versions of conda-build break our symlinks and replace them with
 # copies. This makes the package much larger and also breaks setting up
 # the license key at post-install. conda-build 2.1 seems to be affected
 # but conda-build 2.0.10 seems OK.
+
+# No symlinks in the Windows package
+if sys.platform == 'win32':
+    sys.exit(0)
 
 p = os.path.join(os.environ['SP_DIR'], 'modeller')
 assert(os.path.islink(p) and os.path.exists(p))
