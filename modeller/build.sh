3b13fbf11ac6178192d4d876f9aada302e3cca0e
Ben Webb <ben@salilab.org>
2017-02-22 12:58:35 -0800

    Fix path to HDF5.

--- modeller/build.sh
+++ modeller/build.sh
@@ -29,7 +29,7 @@ if [ `uname -s` = "Darwin" ]; then
     done
     # Point to HDF5 libs in conda package
     for lib in hdf5_hl.10 hdf5.10; do
-      install_name_tool -change /Library/${PKG_NAME}-${PKG_VERSION}/lib/${univ_exetype}/lib${lib}.dylib ${PREFIX}/lib/hdf5-1816/lib${lib}.dylib ${bin}
+      install_name_tool -change /Library/${PKG_NAME}-${PKG_VERSION}/lib/${univ_exetype}/lib${lib}.dylib ${PREFIX}/lib/hdf5-1817/lib${lib}.dylib ${bin}
     done
   done
 
