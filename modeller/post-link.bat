0798e58ffd2c1be17ed72f47bf3f275d0ac609f2
Ben Webb <ben@salilab.org>
2017-02-22 16:10:23 -0800

    Don't output to stdout.
    conda post-link scripts are not supposed to print anything
    when they run. Any output is supposed to go into a
    specially-named file instead.

--- modeller/post-link.bat
+++ modeller/post-link.bat
@@ -1,13 +1,14 @@
 @echo off
 set MODTOP=%PREFIX%\Library\modeller
 set CONFIG=%MODTOP%\modlib\modeller\config.py
+set OUT=%PREFIX%\.messages.txt
 
 if "%KEY_MODELLER%" == "" (
-  echo(
-  echo Edit %CONFIG%
-  echo and replace XXXX with your Modeller license key,
-  echo or set the KEY_MODELLER environment variable before running 'conda install'.
-  echo(
+  echo( > "%OUT%"
+  echo Edit %CONFIG% >> "%OUT%"
+  echo and replace XXXX with your Modeller license key, >> "%OUT%"
+  echo or set the KEY_MODELLER environment variable before running 'conda install'. >> "%OUT%"
+  echo( >> "%OUT%"
 ) else (
   echo install_dir = r'%MODTOP%' > "%CONFIG%"
   echo license = r'%KEY_MODELLER%' >> "%CONFIG%"
