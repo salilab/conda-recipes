c4bd49549135de8951ffe9099bf5225b2b6d6311
Ben Webb <ben@salilab.org>
2017-10-04 13:06:29 -0700

    Use CentOS 6 to build Linux packages.
    Anaconda now requires CentOS 6 or later, and several
    packages no longer work on CentOS 5 (they need a newer
    glibc), so we should update our build environment to
    CentOS 6 too.

--- build/linux-32/setup.sh
+++ build/linux-32/setup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-CONFIG=epel-5-i386
+CONFIG=epel-6-i386
 
 mock -r $CONFIG --init || exit 1
-mock -r $CONFIG --install gcc-c++ make bzip2 wget openssh-clients perl gcc44 gcc44-c++ pkgconfig vim-common || exit 1
+mock -r $CONFIG --install gcc-c++ make bzip2 wget openssh-clients perl pkgconfig vim-common || exit 1
 wget https://repo.continuum.io/miniconda/Miniconda3-4.3.21-Linux-x86.sh && echo "f6a3190b1ada3f7d7a0eb8080cc927216d6c910f2adb5ffdc21817cb71a4fe68  Miniconda3-4.3.21-Linux-x86.sh" | sha256sum -c --status || exit 1
 mock -r $CONFIG --copyin Miniconda3-4.3.21-Linux-x86.sh / || exit 1
 mock -r $CONFIG --chroot "bash /Miniconda3-4.3.21-Linux-x86.sh -b -p /root/miniconda && rm -f /Miniconda3-4.3.21-Linux-x86.sh" || exit 1
