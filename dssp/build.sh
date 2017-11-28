e0cad5e01f3716da25843daedce586bf8715a01e
Ben Webb <ben@salilab.org>
2017-11-28 14:49:39 -0800

    Update to latest DSSP and dependencies
    Update to the latest upstream version (3.0.0) of DSSP,
    and rebuild against the latest version of Boost. Pin all
    dependencies so things don't break next time zlib or bzip2
    are updated. (Note that this won't currently work on Mac
    since DSSP 3 needs C++0x, but that conflicts with the conda
    Boost package.) Closes #3.

--- dssp/build.sh
+++ dssp/build.sh
@@ -9,6 +9,9 @@ echo "DEST_DIR=${PREFIX}" > make.config
 echo "MAN_DIR=${PREFIX}/share/man/man1" >> make.config
 
 if [ `uname -s` = "Darwin" ]; then
+  # Note that this can't work right now because DSSP needs C++0x
+  # and libc++, but the conda Boost package is linked against the
+  # legacy libstdc++ library
   echo "BOOST_INC_DIR=${PREFIX}/include/boost-mac" >> make.config
   echo "BOOST_LIB_DIR=${PREFIX}/lib/boost-mac" >> make.config
   perl -pi -e "s#^(CFLAGS.*)#\$1 -I${PREFIX}/include#" makefile
@@ -16,6 +19,8 @@ if [ `uname -s` = "Darwin" ]; then
 else
   echo "BOOST_INC_DIR=${PREFIX}/include" >> make.config
   echo "BOOST_LIB_DIR=${PREFIX}/lib" >> make.config
+  # Need support for C++0x
+  perl -pi -e 's/^CXX.*/CXX=g++ -std=c++0x/' makefile
 fi
 
 make -j4 install
