642f266db6123341285bebef82b2493be4a42ba1
Ben Webb <ben@salilab.org>
2018-01-09 13:34:24 -0800

    Update to latest Miniconda3

--- build/linux-32/setup.sh
+++ build/linux-32/setup.sh
@@ -4,10 +4,10 @@ CONFIG=epel-6-i386
 
 mock -r $CONFIG --init || exit 1
 mock -r $CONFIG --install gcc-c++ make bzip2 wget openssh-clients perl pkgconfig vim-common || exit 1
-wget https://repo.continuum.io/miniconda/Miniconda3-4.3.27.1-Linux-x86.sh && echo "15fb3364174544d16f452f50eedc32a8a90e27d2fccddff7313654259322105b  Miniconda3-4.3.27.1-Linux-x86.sh" | sha256sum -c --status || exit 1
-mock -r $CONFIG --copyin Miniconda3-4.3.27.1-Linux-x86.sh / || exit 1
-mock -r $CONFIG --chroot "bash /Miniconda3-4.3.27.1-Linux-x86.sh -b -p /root/miniconda && rm -f /Miniconda3-4.3.27.1-Linux-x86.sh" || exit 1
-rm -f Miniconda3-4.3.27.1-Linux-x86.sh
+wget https://repo.continuum.io/miniconda/Miniconda3-4.3.31-Linux-x86.sh && echo "df552bb9046db1f4b68c048e2693dcf52f936de63481589fbaa23c4d161562cf  Miniconda3-4.3.31-Linux-x86.sh" | sha256sum -c --status || exit 1
+mock -r $CONFIG --copyin Miniconda3-4.3.31-Linux-x86.sh / || exit 1
+mock -r $CONFIG --chroot "bash /Miniconda3-4.3.31-Linux-x86.sh -b -p /root/miniconda && rm -f /Miniconda3-4.3.31-Linux-x86.sh" || exit 1
+rm -f Miniconda3-4.3.31-Linux-x86.sh
 mock -r $CONFIG --chroot 'echo "export PATH=\"/root/miniconda/bin:\$PATH\"" >> /builddir/.bashrc' || exit 1
 mock -r $CONFIG --chroot 'source /builddir/.bashrc && conda update -y --all && conda install -y conda-build curl git expat openssl && conda config --add channels salilab' || exit 1
 
