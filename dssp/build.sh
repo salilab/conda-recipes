d0af0a6b83f2507f6fdae93eac569dd3bf1196ae
Ben Webb <ben@salilab.org>
2018-04-25 12:28:38 -0700

    Rebuild with latest gcc and boost

--- dssp/build.sh
+++ dssp/build.sh
@@ -8,19 +8,9 @@ perl -pi -e 's/\-static //' makefile
 echo "DEST_DIR=${PREFIX}" > make.config
 echo "MAN_DIR=${PREFIX}/share/man/man1" >> make.config
 
-if [ `uname -s` = "Darwin" ]; then
-  # Note that this can't work right now because DSSP needs C++0x
-  # and libc++, but the conda Boost package is linked against the
-  # legacy libstdc++ library
-  echo "BOOST_INC_DIR=${PREFIX}/include/boost-mac" >> make.config
-  echo "BOOST_LIB_DIR=${PREFIX}/lib/boost-mac" >> make.config
-  perl -pi -e "s#^(CFLAGS.*)#\$1 -I${PREFIX}/include#" makefile
-  perl -pi -e "s#^(LDOPTS.*)#\$1 -L${PREFIX}/lib#" makefile
-else
-  echo "BOOST_INC_DIR=${PREFIX}/include" >> make.config
-  echo "BOOST_LIB_DIR=${PREFIX}/lib" >> make.config
-  # Need support for C++0x
-  perl -pi -e 's/^CXX.*/CXX=g++ -std=c++0x/' makefile
-fi
+echo "BOOST_INC_DIR=${BUILD_PREFIX}/include" >> make.config
+echo "BOOST_LIB_DIR=${BUILD_PREFIX}/lib" >> make.config
+# Use conda-provided cxx
+perl -pi -e 's/^CXX/#CXX/' makefile
 
 make -j4 install
