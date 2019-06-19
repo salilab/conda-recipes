929efe173b43c5e805d286262e1ef7d33fcd4f8a
Ben Webb <ben@salilab.org>
2019-06-18 22:32:48 -0700

    Fix Python2 library path for Windows

--- imp/bld.bat
+++ imp/bld.bat
@@ -27,6 +27,11 @@ copy "%RECIPE_DIR%\app_wrapper.c" .
 cl app_wrapper.c shell32.lib
 if errorlevel 1 exit 1
 
+:: add path to Python .lib for Python 2.7 builds to help build python-ihm
+if "%PY3K%" == "0" (
+  set OLDPYTHON="-DPYTHON_LIBRARIES=%PREFIX:\=/%/libs/python27.lib"
+)
+
 mkdir build
 cd build
 
@@ -69,7 +74,7 @@ if "%PY3K%" == "1" (
   set NUMPY_INC="-DPython3_NumPy_INCLUDE_DIR=%BUILD_PREFIX%\lib\site-packages\numpy\core\include"
 )
 
-cmake -DUSE_PYTHON2=%USE_PYTHON2% %NUMPY_INC% -DCMAKE_PREFIX_PATH="%BUILD_PREFIX:\=/%;%BUILD_PREFIX:\=/%\Library" -DCMAKE_BUILD_TYPE=Release -DIMP_DISABLED_MODULES=scratch -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" -DCMAKE_INSTALL_PYTHONDIR="%SP_DIR%" -DCMAKE_CXX_FLAGS="/DBOOST_ALL_DYN_LINK /EHsc /D_HDF5USEDLL_ /DH5_BUILT_AS_DYNAMIC_LIB /DPROTOBUF_USE_DLLS /DWIN32 /DGSL_DLL /DMSMPI_NO_DEPRECATE_20 %EXTRA_CXX_FLAGS%" -Dopencv_core_LIBRARY="%BUILD_PREFIX:\=/%/Library/lib/opencv_core%OPENCV_VER%.lib" -Dopencv_imgproc_LIBRARY="%BUILD_PREFIX:\=/%/Library/lib/opencv_imgproc%OPENCV_VER%.lib" -Dopencv_highgui_LIBRARY="%BUILD_PREFIX:\=/%/Library/lib/opencv_highgui%OPENCV_VER%.lib" -Dopencv_imgcodecs_LIBRARY="%BUILD_PREFIX:\=/%/Library/lib/opencv_imgcodecs%OPENCV_VER%.lib" -DHDF5_LIBRARIES="%BUILD_PREFIX:\=/%/Library/lib/hdf5.lib" -DHDF5_FOUND=TRUE -DHDF5_INCLUDE_DIRS="%BUILD_PREFIX:\=/%/Library/include" -DHDF5_INCLUDE_DIR="%BUILD_PREFIX:\=/%/Library/include" -Dprotobuf_LIBRARY="%BUILD_PREFIX:\=/%/Library/lib/libprotobuf.lib" %PERCPPCOMP% -G "NMake Makefiles" ..
+cmake -DUSE_PYTHON2=%USE_PYTHON2% %OLDPYTHON% %NUMPY_INC% -DCMAKE_PREFIX_PATH="%BUILD_PREFIX:\=/%;%BUILD_PREFIX:\=/%\Library" -DCMAKE_BUILD_TYPE=Release -DIMP_DISABLED_MODULES=scratch -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" -DCMAKE_INSTALL_PYTHONDIR="%SP_DIR%" -DCMAKE_CXX_FLAGS="/DBOOST_ALL_DYN_LINK /EHsc /D_HDF5USEDLL_ /DH5_BUILT_AS_DYNAMIC_LIB /DPROTOBUF_USE_DLLS /DWIN32 /DGSL_DLL /DMSMPI_NO_DEPRECATE_20 %EXTRA_CXX_FLAGS%" -Dopencv_core_LIBRARY="%BUILD_PREFIX:\=/%/Library/lib/opencv_core%OPENCV_VER%.lib" -Dopencv_imgproc_LIBRARY="%BUILD_PREFIX:\=/%/Library/lib/opencv_imgproc%OPENCV_VER%.lib" -Dopencv_highgui_LIBRARY="%BUILD_PREFIX:\=/%/Library/lib/opencv_highgui%OPENCV_VER%.lib" -Dopencv_imgcodecs_LIBRARY="%BUILD_PREFIX:\=/%/Library/lib/opencv_imgcodecs%OPENCV_VER%.lib" -DHDF5_LIBRARIES="%BUILD_PREFIX:\=/%/Library/lib/hdf5.lib" -DHDF5_FOUND=TRUE -DHDF5_INCLUDE_DIRS="%BUILD_PREFIX:\=/%/Library/include" -DHDF5_INCLUDE_DIR="%BUILD_PREFIX:\=/%/Library/include" -Dprotobuf_LIBRARY="%BUILD_PREFIX:\=/%/Library/lib/libprotobuf.lib" %PERCPPCOMP% -G "NMake Makefiles" ..
 if errorlevel 1 exit 1
 
 nmake install
