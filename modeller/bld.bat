9739c333f15fa895542241abc3db61b420997e97
Ben Webb <ben@salilab.org>
2019-10-23 10:11:33 -0700

    Bundle VS 2010 runtime with 32-bit package
    Anaconda used to provide a vs2010_runtime package
    which included the distributable DLLs from MSVS 2010.
    Since this is no longer available, bundle the DLL we
    need with the Modeller package.

--- modeller/bld.bat
+++ modeller/bld.bat
@@ -13,14 +13,15 @@ if errorlevel 1 exit 1
 
 :: Install bundled DLLs but rename them to avoid potential conflicts with
 :: other Anaconda packages (e.g. the conda hdf5 package). We do this because
-:: DLLs either need to be in the PATH or in the same directory as the binary
-:: that uses them. We can't do the latter because the modXXX binary, the
-:: libmodeller DLL, and the two identically named _modeller.pyd extensions
-:: cannot all be in the same directory, and if we put the DLLs in the PATH, they
-:: are visible globally and could conflict.
+:: DLLs either need to be in the PATH (at least prior to Python 3.8) or in
+:: the same directory as the binary that uses them. We can't do the latter
+:: because the modXXX binary, the libmodeller DLL, and the two identically
+:: named _modeller.pyd extensions cannot all be in the same directory,
+:: and if we put the DLLs in the PATH, they are visible globally and
+:: could conflict.
 if "%ARCH%" == "32" (
-  :: 32-bit uses msvcr100, which is provided by the conda vs2010_runtime package
-  python "%RECIPE_DIR%\install_bins.py" lib\%EXETYPE% "%PREFIX%" iconv.dll intl.dll hdf5.dll hdf5_hl.dll libglib-2.0-0.dll zlib1.dll libifcoremd.dll libmmd.dll --norename libmodeller%SOVERSION%.dll libsaxs.dll mod%PKG_VERSION%.exe python%PY_VER%\_modeller.pyd python2.3\_modeller23.pyd
+  :: 32-bit uses msvcr100, so must pull that in (not in Anaconda)
+  python "%RECIPE_DIR%\install_bins.py" lib\%EXETYPE% "%PREFIX%" iconv.dll intl.dll hdf5.dll hdf5_hl.dll libglib-2.0-0.dll zlib1.dll libifcoremd.dll libmmd.dll --norename msvcr100.dll libmodeller%SOVERSION%.dll libsaxs.dll mod%PKG_VERSION%.exe python%PY_VER%\_modeller.pyd python2.3\_modeller23.pyd
   if errorlevel 1 exit 1
 ) else (
   :: 64-bit uses msvcr110, so must pull that in (not in Anaconda)
