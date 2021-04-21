7f6f7a342b46eb8790c4ba6c8b01a572456b87f8
Ben Webb <ben@salilab.org>
2021-04-21 16:46:07 -0700

    Test HDF5 support

--- conda-forge/rmf/run_test.py
+++ conda-forge/rmf/run_test.py
@@ -2,12 +2,14 @@ import RMF
 import os
 
 # Make sure that we can read in an RMF file that we ourselves created
-r = RMF.create_rmf_file("test.rmf")
-r.add_frame("root", RMF.FRAME)
-del r
+# in both modern and old (HDF5) format
+for suffix in ('rmf', 'rmf-hdf5'):
+    r = RMF.create_rmf_file("test.%s" % suffix)
+    r.add_frame("root", RMF.FRAME)
+    del r
 
-r = RMF.open_rmf_file_read_only("test.rmf")
-r.set_current_frame(RMF.FrameID(0))
-del r
+    r = RMF.open_rmf_file_read_only("test.%s" % suffix)
+    r.set_current_frame(RMF.FrameID(0))
+    del r
 
-os.unlink("test.rmf")
+    os.unlink("test.%s" % suffix)
