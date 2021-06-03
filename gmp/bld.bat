22570330ed942e9e6c539b2f60c857ad41b3da72
Ben Webb <ben@salilab.org>
2021-06-03 13:44:24 -0700

    Remove IMP build dependencies
    We now rely on conda-forge for dependencies
    such as CGAL (and its dependencies), so no
    need to maintain our own copies any more.

--- gmp/bld.bat
+++
@@ -1,8 +0,0 @@
-copy include\*.h "%LIBRARY_INC%\"
-if errorlevel 1 exit 1
-
-copy lib\*.dll "%LIBRARY_BIN%\"
-if errorlevel 1 exit 1
-
-copy lib\*.lib "%LIBRARY_LIB%\"
-if errorlevel 1 exit 1
--- gsl/make_proj_64bit.py
+++
@@ -1,8 +0,0 @@
-# Hack project files to work on 64 bit
-import sys
-
-fname = sys.argv[1]
-
-contents = open(fname).read()
-contents = contents.replace('Win32', 'x64')
-open(fname, 'w').write(contents)
