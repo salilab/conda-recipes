96ac655b5688cc5c7e0c81e6c1ad6968bf21df4e
Ben Webb <ben@salilab.org>
2023-04-03 16:39:03 -0700

    Don't require perl to build DSSP

--- dssp/build.sh
+++ dssp/build.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Link dynamically
-perl -pi -e 's/\-static //' makefile
+sed -i -e 's/\-static //' makefile
 
 echo "DEST_DIR=${PREFIX}" > make.config
 echo "MAN_DIR=${PREFIX}/share/man/man1" >> make.config
@@ -11,7 +11,7 @@ echo "MAN_DIR=${PREFIX}/share/man/man1" >> make.config
 echo "BOOST_INC_DIR=${BUILD_PREFIX}/include" >> make.config
 echo "BOOST_LIB_DIR=${BUILD_PREFIX}/lib" >> make.config
 # Use conda-provided cxx
-perl -pi -e 's/^CXX/#CXX/' makefile
+sed -i -e 's/^CXX/#CXX/' makefile
 
 # clang++ won't find std::tuple unless in C++11 mode
 if [ `uname -s` = "Darwin" ]; then
